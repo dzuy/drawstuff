894e9e7b578213c6fde7fa474cc6e209b6e7ee34
Dzuy Linh <dzuylinh@gmail.com>
2013-10-12 13:59:55 -0700

    Initial Commit

---
+++ DrawStuff/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DrawStuff
+//
+//  Created by Dzuy Linh on 10/12/13.
+//  Copyright (c) 2013 Dzuy Linh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DrawStuff/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DrawStuff
+//
+//  Created by Dzuy Linh on 10/12/13.
+//  Copyright (c) 2013 Dzuy Linh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
